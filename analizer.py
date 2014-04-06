0b0a8b3f684b8d3be31572d6fd8f8b7c118b607b
Mindaugas - laptop <mseskas@gmail.com>
2014-04-06 17:47:46 +0300

    implemented analizer.analize_single_file method

--- analizer.py
+++ analizer.py
@@ -1,23 +1,52 @@
 from statistics_types import file_statistics
 import os
 
+
 def analize_dir(path):
-    list_of_file = os.listdir(path)
+    list_of_obj = os.listdir(path)
+    list_of_files = list()
+    for obj in list_of_files:
+        if (os.path.isfile(obj) is True):
+            list_of_files.append(obj)
+
     list_of_statistics = list()
-    for item in list_of_file:
-        list_of_statistics = analize_single_file(item)
-    
-    total = add_lists(list_of_statistics)
+    for item in list_of_files:
+        list_of_statistics.append(analize_single_file(path, item))
+
+    total = total_statistics(list_of_statistics)
     write_statistics(total, list_of_statistics, "result.txt")
 
 
-def analize_single_file(path):
-    print "not implemented analize_single_file function"
+def analize_single_file(path, name):
+    curr_file = open(path + "/" + name, "r")
+    content = curr_file.read()
+    array_char = list(content)
+    dic_char = dict()
+    for char in array_char:
+        if (dic_char.get(char) is not None):
+            dic_char[char] = dic_char[char] + 1
+        else:
+            dic_char[char] = 1
+
+    array_char = None
+    array_word = content.split()  # split to word including newLine
+    dic_word = dict()
+    for word in array_word:
+        if (dic_word.get(word) is not None):
+            dic_word[word] = dic_word[word] + 1
+        else:
+            dic_word[word] = 1
+
+    statistics = file_statistics(name)
+    statistics.words_dic = dic_word
+    statistics.char_dic = dic_char
+    return statistics
 
 
-def add_lists(list_of_lists):
+def total_statistics(list_of_lists):
     print "not implemented add_lists function"
 
 
 def write_statistics(total, list_of_statistics, file_name):
+    # list().sort()
     print "not implemented write_statistics function"
--- statistics_types.py
+++ statistics_types.py
@@ -1,8 +1,11 @@
 # class that holds file's statistics
 class file_statistics:
-   __file_name = ""
-   words_list = None
-   symbols_list = None
+    __file_name__ = ""
+    words_dic = None
+    char_dic = None
 
-   def __init__(self, name):
-       self.__file_name = name
+    def __init__(self, name):
+        self.__file_name__ = name
+
+    def get_file_name(self):
+        return self.__file_name__
