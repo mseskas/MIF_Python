d358927d6b2f06fdb08aad5ae4acbee3c8823985
Mindaugas - laptop <mseskas@gmail.com>
2014-04-28 14:29:34 +0300

    added files

---
+++ OpenCV and Python/3floor_e_adaptive.py
@@ -0,0 +1,44 @@
+
+
+import cv2.cv as cv
+import cv2
+
+# import numpy as np
+
+import sys
+
+#import pdb
+#pdb.set_trace()
+
+
+cv.NamedWindow("camera", 1)
+cv.NamedWindow("thr", 1)
+
+# capture = cv.CaptureFromCAM(0)
+
+
+# img = cv.QueryFrame(capture)
+
+#img = cv2.imread("./b.jpg")
+img = cv2.imread("./e.jpg")
+img = cv.fromarray(img)
+
+gray = cv.CreateImage((img.width, img.height), 8, 1)
+dst = cv.CreateImage((img.width, img.height), 8, 1)
+# dst = cv.CreateImage((img.width, img.height), 8, 1)
+
+cv.CvtColor(img, gray, cv.CV_BGR2GRAY)
+
+cv.AdaptiveThreshold(gray, dst, maxValue=255,
+    adaptive_method=cv.CV_ADAPTIVE_THRESH_MEAN_C)
+
+
+cv.ShowImage("thr", dst)
+cv.ShowImage("camera", img)
+
+cv.WaitKey(0)
+cv.DestroyAllWindows()
+quit()
+
+
+
