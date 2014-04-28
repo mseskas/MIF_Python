785be51de8b6a3f9bc0370449850a511f80cb379
Mindaugas - laptop <mseskas@gmail.com>
2014-04-28 16:05:43 +0300

    last

--- Seminaras/2floor_d_adaptive.py
+++ Seminaras/2floor_d_adaptive.py
@@ -1,22 +1,12 @@
-
-
 import cv2.cv as cv
 import cv2
 
-# import numpy as np
-
 import sys
-
-#import pdb
-#pdb.set_trace()
-
 
 cv.NamedWindow("camera", 1)
 cv.NamedWindow("thr", 1)
 
 # capture = cv.CaptureFromCAM(0)
-
-
 # img = cv.QueryFrame(capture)
 
 img = cv2.imread("./d.jpg")
@@ -24,12 +14,14 @@ img = cv.fromarray(img)
 
 gray = cv.CreateImage((img.width, img.height), 8, 1)
 dst = cv.CreateImage((img.width, img.height), 8, 1)
-# dst = cv.CreateImage((img.width, img.height), 8, 1)
 
 cv.CvtColor(img, gray, cv.CV_BGR2GRAY)
 
 cv.AdaptiveThreshold(gray, dst, maxValue=255,
-    adaptive_method=cv.CV_ADAPTIVE_THRESH_MEAN_C)
+    adaptive_method=cv.CV_ADAPTIVE_THRESH_GAUSSIAN_C)
+
+# CV_ADAPTIVE_THRESH_GAUSSIAN_C 
+# CV_ADAPTIVE_THRESH_MEAN_C
 
 
 cv.ShowImage("thr", dst)
--- Seminaras/2floor_d_thresh.py
+++ Seminaras/2floor_d_thresh.py
@@ -13,15 +13,14 @@ cv.NamedWindow("thr", 1)
 img = cv2.imread("./d.jpg")
 img = cv.fromarray(img)
 
-
-# gray = cv.CreateImage((img.width, img.height), cv.IPL_DEPTH_8U, 1)
 dst = cv.CreateImage((img.width, img.height), cv.IPL_DEPTH_8U, 1)
 hsv = cv.CreateImage((img.width, img.height), cv.IPL_DEPTH_8U, 3)
 
 cv.CvtColor(img, hsv, cv.CV_BGR2HSV)
-# cv.CvtColor(img, gray, cv.CV_BGR2GRAY)
 
-cv.InRangeS(hsv, cv.Scalar(0,100,0), cv.Scalar(180,255,255), dst)
+#cv.InRangeS(hsv, cv.Scalar(0,100,0), cv.Scalar(180,255,255), dst)
+#cv.InRangeS(hsv, [0,100,0], [180,255,255], dst)
+cv.InRangeS(hsv, (0,100,0), (180,255,255), dst)
 
 cv.ShowImage("thr", dst)
 cv.ShowImage("camera", hsv)
@@ -29,3 +28,14 @@ cv.ShowImage("camera", hsv)
 cv.WaitKey(0)
 cv.DestroyAllWindows()
 quit()
+
+"""
+dst = cv.CreateImage((img.width, img.height), cv.IPL_DEPTH_8U, 1)
+
+cv.CvtColor(img, img, cv.CV_BGR2HSV)
+
+cv.InRangeS(img, (0,100,0), (180,255,255), dst)
+
+cv.ShowImage("thr", dst)
+cv.ShowImage("camera", img)
+"""
